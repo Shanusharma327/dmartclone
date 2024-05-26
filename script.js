5a9d949eaea72b8c8c9e2a6d0b90a8339696c038
Shanusharma327 <124282670+Shanusharma327@users.noreply.github.com>
2024-05-26 13:59:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let searchForm=document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+    Menu.classList.remove('active');
+    ShoppingCart.classList.remove('active');
+    Login.classList.remove('active');
+}
+
+let Menu=document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick=()=>{
+    Menu.classList.toggle('active');
+    searchForm.classList.remove('active');
+    ShoppingCart.classList.remove('active');
+    Login.classList.remove('active');
+}
+window.onscroll=()=>{
+    Menu.classList.remove('active');
+    searchForm.classList.remove('active');
+    ShoppingCart.classList.remove('active');
+    Login.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".product-slider", {
+  loop:true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+
+    },
+    750: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
